853e0c3108d8a513977099d5a6b3a1c9c028d933
Naim As Sakib <sakib.rifat90@gmail.com>
2023-02-19 22:05:07 +0600

    adding multiple layouts in a single parent layout

--- app/src/main/java/com/example/demolayouts/MainActivity.kt
+++ app/src/main/java/com/example/demolayouts/MainActivity.kt
@@ -11,9 +11,6 @@ import com.example.demolayouts.databinding.SecondLayoutBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
 
-  /*  private lateinit var binding1: FirstLayoutBinding
-
-   private lateinit var binding2: SecondLayoutBinding*/
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,23 +19,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*binding1 = FirstLayoutBinding.inflate(layoutInflater)
-        binding.root.addView(binding1.root)
-
-        binding2 = SecondLayoutBinding.inflate(layoutInflater)
-        binding.root.addView(binding2.root)*/
-
-      /*
-        binding1.tvLayout1.text = "Hello from the first layout!"
-        binding2.tvLayout2.text = "Hello from the second layout!"*/
-
-        // Set the visibility of views in the main layout
-        binding.mainFrameLayout.addView(layoutInflater.inflate(R.layout.second_layout, binding.mainFrameLayout, false))
-
-        binding.mainFrameLayout.removeAllViews()
-
-        binding.mainFrameLayout.addView(layoutInflater.inflate(R.layout.first_layout, binding.mainFrameLayout, false))
 
 
+
+        binding.layout1InInclude.root.visibility=View.VISIBLE
+        binding.layout2InInclude.root.visibility=View.GONE
     }
 }
